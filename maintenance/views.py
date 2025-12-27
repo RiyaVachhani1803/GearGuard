156d29c64fa6b41ef5276c1195b36619bca7e3e1
Neel <neelgundale@gmail.com>
2025-12-27 16:24:27 +0530

    Add maintenance files

--- maintenance/views.py
+++ maintenance/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import MaintenanceRequest
 from .models import Equipment, Team
 
+from django.contrib.auth.decorators import login_required
+
+@login_required(login_url="login")
 def dashboard(request):
     return render(request, "maintenance/home.html")
 
