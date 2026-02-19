cd8ed7de61067efd880b4edd5fd54e33beb1425c
Neel Gundale <neelgundale@gmail.com>
2026-02-19 22:59:14 +0530

    Delete maintenance directory

--- maintenance/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Team, Equipment, MaintenanceRequest
-
-admin.site.register(Team)
-admin.site.register(Equipment)
-admin.site.register(MaintenanceRequest)
